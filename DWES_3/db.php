d1d715bd253df7935b08f330e98ffc85cdbc6d84
vbmh56 <vbmh56@educa.madrid.org>
2026-01-09 20:13:13 +0100

    Added a few things for DWES 3

---
+++ DWES_3/db.php
@@ -0,0 +1,19 @@
+<?php
+// app/config/db.php
+
+function getPDO(): PDO {
+    $host = 'localhost';
+    $db   = 'proyecto';
+    $user = 'gestor';
+    $pass = 'secreto';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // errores claros
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // arrays asociativos
+        // PDO::ATTR_EMULATE_PREPARES   => false,                  // prepares reales
+    ];
+
+    return new PDO($dsn, $user, $pass, $options);
+}
